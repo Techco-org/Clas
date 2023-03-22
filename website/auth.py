1d2eccf13470c649278155e429ec57aa26acde9e
ltvinh <everwellmax@gmail.com>
2023-03-22 22:57:50 +0700

    authetication

--- website/auth.py
+++ website/auth.py
@@ -1,7 +1,66 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, session, redirect, url_for
+from flask_login import login_user, login_remembered, current_user, login_required, logout_user
+from . import db
+from .models import User
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/')
 def index():
     return render_template('index.html')
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if login_remembered():
+        return redirect(url_for('views.home'))
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        remember = request.form.get('remember')
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if password == user.password:
+                if remember is None:
+                    login_user(user, remember=False)
+                else:
+                    login_user(user, remember=True)
+                flash('Log in successfully!', category='success')
+                return redirect(url_for('views.home'))
+            else: 
+                flash('Incorrect password, try again', category='error')
+        else:
+            flash('Email does not exist', category='error')
+        return redirect(url_for('auth.login'))
+    return render_template('login.html', user=current_user)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
+
+@auth.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        reconfirm = request.form.get('reconfirm')
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists', category='error')
+        elif len(username) < 4:
+            flash('Username is too short', category='error')
+        elif len(email) < 4:
+            flash('Invalid email', category='error')
+        elif len(password) < 8:
+            flash('Password is too short', category='error')
+        elif password != reconfirm:
+            flash('Password don\'t match', category='error')
+        else:
+            new_user = User(username=username, email=email, password=password)
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Account created', category='success')
+        return redirect(url_for('auth.signup'))
+    return render_template('signup.html')

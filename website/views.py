e7694c279a7656e4e3e4d25cd589e6da2685a5cc
ltvinh <everwellmax@gmail.com>
2023-03-25 22:59:00 +0700

    added edit-personal route

--- website/views.py
+++ website/views.py
@@ -10,9 +10,21 @@ views = Blueprint('views', __name__)
 @views.route('/')
 @login_required
 def home():
-    return render_template('home.html')
+    return render_template('home.html' , user=current_user)
 
 @views.route('/profile')
 @login_required
 def profile():
-    return render_template('profile.html')
+    return render_template('profile.html', user=current_user)
+
+@views.route('/profile/edit-personal', methods=['GET', 'POST'])
+@login_required
+def edit_personal():
+    if request.method == 'POST':
+        current_user.fullname = request.form.get('name')
+        current_user.bio = request.form.get('bio')
+        current_user.city = request.form.get('city')
+        current_user.country = request.form.get('country')
+        current_user.url = request.form.get('url')
+        db.session.commit()
+        return redirect(url_for('views.profile'))

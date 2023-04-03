de31bad2eef24eb5957cb15159ec1545028ae5cd
ltvinh <everwellmax@gmail.com>
2023-04-03 23:49:26 +0700

    modified

--- website/data.py
+++ website/data.py
@@ -3,7 +3,7 @@ from .models import Img
 
 data = Blueprint('data', __name__)
 
-@data.route('/<user_id>_avatar')
+@data.route('/<user_id>/avatar')
 def get_avatar_image(user_id):
     img = Img.query.filter_by(name=str(user_id)+'-avatar').first()
     if not img:
@@ -11,7 +11,7 @@ def get_avatar_image(user_id):
     
     return Response(img.img, mimetype=img.mimetype)
 
-@data.route('/<user_id>_background')
+@data.route('/<user_id>/background')
 def get_background_image(user_id):
     img = Img.query.filter_by(name=str(user_id)+'-background').first()
     if not img:
--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for, Response
+from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import login_required, current_user
 
 from datetime import datetime
@@ -8,57 +8,19 @@ from .models import User, Task, Img, Grade, Licer
 
 views = Blueprint('views', __name__)
 
+#Index route
 @views.route('/')
 @login_required
 def home():
     return redirect(url_for('views.profile'))
 
+#Profile group
 @views.route('/profile')
 @login_required
 def profile():
     return render_template('profile.html', user=current_user)
 
-@views.route('/tasks')
-@login_required
-def tasks():
-    current_date = datetime.now().strftime("%B %d, %Y")
-    return render_template('tasks.html', current_date=current_date, user=current_user)
-
-@views.route('/tasks/new', methods=['GET', 'POST'])
-@login_required
-def add_task():
-    if request.method == 'POST':
-        task_title = request.form.get('task-title')
-        task_data = request.form.get('task-data')
-
-        new_task = Task(title=task_title, data=task_data, user_id=current_user.id)
-        db.session.add(new_task)
-        db.session.commit()
-    return redirect(url_for('views.tasks'))
-
-
-@views.route('/tasks/delete/<task_id>')
-@login_required
-def delete_task(task_id):
-    task = Task.query.filter_by(id=task_id).first()
-    
-    db.session.delete(task)
-    db.session.commit()
-    return redirect(url_for('views.tasks'))
-
-@views.route('/tasks/check/<task_id>')
-@login_required
-def check_task(task_id):
-    task = Task.query.filter_by(id=task_id).first()
-    if task.state == False:
-        task.state = True
-    else:
-        task.state = False
-
-    db.session.commit()
-    return redirect(url_for('views.tasks'))
-
-@views.route('/profile/edit-profile', methods=['GET', 'POST'])
+@views.route('/profile/edit/profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
     if request.method == 'POST':
@@ -71,7 +33,7 @@ def edit_profile():
         db.session.commit()
         return redirect(url_for('views.profile'))
     
-@views.route('/profile/edit-basic-info', methods=['GET', 'POST'])
+@views.route('/profile/edit/basic-info', methods=['GET', 'POST'])
 @login_required
 def edit_basic_info():
     if request.method == 'POST':
@@ -84,8 +46,39 @@ def edit_basic_info():
 
         db.session.commit()
         return redirect(url_for('views.profile'))
+    
+#Profile extentions
+@views.route('/upload/personal-image', methods=['GET', 'POST'])
+@login_required
+def edit_personal_image():
+    if request.method == 'POST':
+        avt_img = request.files['avatar-image-input']
+        bg_img = request.files['background-image-input']
+        if avt_img:
+            exist_avt = Img.query.filter_by(name=str(current_user.id)+'-avatar').first()
+            if not exist_avt:
+                avt_filename = str(current_user.id)+"-avatar"
+                avt_minetype = avt_img.mimetype
+                avt_img = Img(img=avt_img.read(), mimetype=avt_minetype, name=avt_filename)
+                db.session.add(avt_img)
+                db.session.commit()
+            else:
+                exist_avt.img = avt_img.read()
+                db.session.commit()
+        if bg_img:
+            exist_bg = Img.query.filter_by(name=str(current_user.id)+'-background').first()
+            if not exist_bg:
+                bg_filename = str(current_user.id)+"-background"
+                bg_minetype = bg_img.mimetype
+                bg_img = Img(img=bg_img.read(), mimetype=bg_minetype, name=bg_filename)
+                db.session.add(bg_img)
+                db.session.commit()
+            else:
+                exist_bg.img = bg_img.read()
+                db.session.commit()
+        return redirect(url_for('views.profile'))
 
-@views.route('/profile/grade/new', methods=['GET', 'POST'])
+@views.route('/grade/new', methods=['GET', 'POST'])
 @login_required
 @login_required
 def add_grading():
@@ -134,59 +127,55 @@ def delete_licer(licer_id):
     db.session.commit()
     return redirect(url_for('views.profile'))
 
-@views.route('/profile/upload-personal-image', methods=['GET', 'POST'])
+#Tasks group
+@views.route('/tasks')
 @login_required
-def edit_personal_image():
+def tasks():
+    current_date = datetime.now().strftime("%B %d, %Y")
+    return render_template('tasks.html', current_date=current_date, user=current_user)
+
+@views.route('/tasks/new', methods=['GET', 'POST'])
+@login_required
+def add_task():
     if request.method == 'POST':
-        avt_img = request.files['avatar-image-input']
-        bg_img = request.files['background-image-input']
-        if avt_img:
-            exist_avt = Img.query.filter_by(name=str(current_user.id)+'-avatar').first()
-            if not exist_avt:
-                avt_filename = str(current_user.id)+"-avatar"
-                avt_minetype = avt_img.mimetype
-                avt_img = Img(img=avt_img.read(), mimetype=avt_minetype, name=avt_filename)
-                db.session.add(avt_img)
-                db.session.commit()
-            else:
-                exist_avt.img = avt_img.read()
-                db.session.commit()
-        if bg_img:
-            exist_bg = Img.query.filter_by(name=str(current_user.id)+'-background').first()
-            if not exist_bg:
-                bg_filename = str(current_user.id)+"-background"
-                bg_minetype = bg_img.mimetype
-                bg_img = Img(img=bg_img.read(), mimetype=bg_minetype, name=bg_filename)
-                db.session.add(bg_img)
-                db.session.commit()
-            else:
-                exist_bg.img = bg_img.read()
-                db.session.commit()
-        return redirect(url_for('views.profile'))
-    
-@views.route('/<user_id>_avatar')
-def get_avatar_image(user_id):
-    img = Img.query.filter_by(name=str(user_id)+'-avatar').first()
-    if not img:
-        return 'No image with this id', 404
-    
-    return Response(img.img, mimetype=img.mimetype)
+        task_title = request.form.get('task-title')
+        task_data = request.form.get('task-data')
 
-@views.route('/<user_id>_background')
-def get_background_image(user_id):
-    img = Img.query.filter_by(name=str(user_id)+'-background').first()
-    if not img:
-        return 'No image with this id', 404
-    
-    return Response(img.img, mimetype=img.mimetype)
+        new_task = Task(title=task_title, data=task_data, user_id=current_user.id)
+        db.session.add(new_task)
+        db.session.commit()
+    return redirect(url_for('views.tasks'))
 
+
+@views.route('/tasks/delete/<task_id>')
+@login_required
+def delete_task(task_id):
+    task = Task.query.filter_by(id=task_id).first()
+    
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for('views.tasks'))
+
+@views.route('/tasks/check/<task_id>')
+@login_required
+def check_task(task_id):
+    task = Task.query.filter_by(id=task_id).first()
+    if task.state == False:
+        task.state = True
+    else:
+        task.state = False
+
+    db.session.commit()
+    return redirect(url_for('views.tasks'))
+
+#Search data group
 @views.route('/search', methods=['POST', 'GET'])
 def search():
     if request.method == 'POST':
         search_query = request.form.get('search')
     return redirect(url_for('views.search_result', query=search_query))
 
-@views.route('/search-result/<query>')
+@views.route('/search/result/<query>')
 def search_result(query):
     results = User.query.msearch(query).all()
     print(results)
@@ -198,3 +187,9 @@ def user(user_id):
     if not user:
         return 'No user with this id', 404
     return render_template('user.html', user=user) 
+
+#Setting group
+@views.route('/settings')
+@login_required
+def settings():
+    return render_template('settings.html')
